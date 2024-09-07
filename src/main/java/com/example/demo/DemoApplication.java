8d3f3bd411dc3fd7caaabbae33675b8733de8ab1
tanveer <tanveerkhan@gmail.com>
2024-09-07 20:46:14 +0530

    code change

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,17 +8,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 @Controller
-public class StartApplication {
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
     @GetMapping("/")
     public String index(final Model model) {
         model.addAttribute("title", "Welcome");
-        model.addAttribute("msg", "Hello ther");
-        return "index";
+        model.addAttribute("msg", "Hello there");
+        return "index"; // Make sure you have an index.html in src/main/resources/templates
     }
-
-    public static void main(String[] args) {
-        SpringApplication.run(StartApplication.class, args);
-    }
-
 }

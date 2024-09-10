8a76bcb5656e6c0d07f6fb3ad80a4a7da23c087b
tanveer <tanveerkhan@gmail.com>
2024-09-10 19:33:22 +0530

    CD added vpc code in maintf, updated value to enable ingress rules

--- ekscluster/main.tf
+++ ekscluster/main.tf
@@ -11,6 +11,10 @@ provider "aws" {
   region = "us-east-2"
 }
 
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
 # VPC
 resource "aws_vpc" "main" {
   cidr_block           = "10.0.0.0/16"
@@ -64,6 +68,59 @@ resource "aws_route_table_association" "public" {
   count          = 2
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
+}
+
+# Security Group for EKS Nodes
+resource "aws_security_group" "eks_nodes_sg" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "eks-nodes-sg"
+  }
+}
+
+# Security Group for Load Balancer
+resource "aws_security_group" "alb_sg" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "alb-sg"
+  }
 }
 
 # IAM Role for EKS Cluster
@@ -137,6 +194,7 @@ resource "aws_eks_cluster" "eks_cluster" {
 
   vpc_config {
     subnet_ids = aws_subnet.public[*].id
+    security_group_ids = [aws_security_group.alb_sg.id]
   }
 
   depends_on = [
@@ -150,6 +208,7 @@ resource "aws_eks_node_group" "eks_nodes" {
   node_group_name = "example-node-group"
   node_role_arn   = aws_iam_role.eks_node_role.arn
   subnet_ids      = aws_subnet.public[*].id
+  node_security_groups = [aws_security_group.eks_nodes_sg.id]
 
   scaling_config {
     desired_size = 2
@@ -162,7 +221,115 @@ resource "aws_eks_node_group" "eks_nodes" {
   }
 }
 
-# Output the EKS Cluster Name
-output "eks_cluster_name" {
-  value = aws_eks_cluster.eks_cluster.name
+# IAM Role for AWS Load Balancer Controller
+resource "aws_iam_role" "alb_ingress_role" {
+  name = "alb-ingress-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Federated = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/oidc.eks.${aws_eks_cluster.eks_cluster.region}.amazonaws.com/id/${aws_eks_cluster.eks_cluster.id}"
+        },
+        Action = "sts:AssumeRoleWithWebIdentity",
+        Condition = {
+          StringEquals = {
+            "oidc.eks.${aws_eks_cluster.eks_cluster.region}.amazonaws.com/id/${aws_eks_cluster.eks_cluster.id}:sub": "system:serviceaccount:kube-system:alb-ingress-controller"
+          }
+        }
+      }
+    ]
+  })
+
+  tags = {
+    Name = "alb-ingress-role"
+  }
+}
+
+# IAM Policy for AWS Load Balancer Controller
+resource "aws_iam_policy" "alb_ingress_policy" {
+  name        = "alb-ingress-policy"
+  description = "Policy for AWS Load Balancer Controller"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = [
+          "elasticloadbalancing:CreateLoadBalancer",
+          "elasticloadbalancing:DeleteLoadBalancer",
+          "elasticloadbalancing:CreateTargetGroup",
+          "elasticloadbalancing:DeleteTargetGroup",
+          "elasticloadbalancing:RegisterTargets",
+          "elasticloadbalancing:DeregisterTargets",
+          "elasticloadbalancing:DescribeLoadBalancers",
+          "elasticloadbalancing:DescribeTargetGroups",
+          "elasticloadbalancing:DescribeTargetHealth",
+          "ec2:DescribeSecurityGroups",
+          "ec2:DescribeSubnets",
+          "ec2:DescribeVpcs",
+          "iam:GetPolicy",
+          "iam:GetPolicyVersion",
+          "iam:GetRole",
+          "iam:GetRolePolicy",
+          "iam:GetUser",
+          "iam:ListAttachedRolePolicies",
+          "iam:ListAttachedUserPolicies",
+          "iam:ListRolePolicies",
+          "iam:ListRoleTags",
+          "iam:ListRoles",
+          "iam:ListRolePolicies",
+          "iam:ListAttachedRolePolicies",
+          "iam:ListAttachedUserPolicies",
+          "iam:ListRoleTags"
+        ],
+        Effect = "Allow",
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "alb_ingress_policy_attachment" {
+  policy_arn = aws_iam_policy.alb_ingress_policy.arn
+  role      = aws_iam_role.alb_ingress_role.name
+}
+
+# Load Balancer Controller Installation via Helm (Assuming you use Helm)
+resource "helm_release" "alb_ingress_controller" {
+  name       = "aws-load-balancer-controller"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  version    = "1.6.8"  # Use the latest version as appropriate
+
+  set {
+    name  = "clusterName"
+    value = aws_eks_cluster.eks_cluster.name
+  }
+
+  set {
+    name  = "serviceAccount.create"
+    value = "false"
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = "alb-ingress-controller"
+  }
+
+  set {
+    name  = "aws.region"
+    value = "us-east-2"
+  }
+
+  set {
+    name  = "image.tag"
+    value = "v2.2.0"  # Use the latest version as appropriate
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.alb_ingress_policy_attachment
+  ]
 }
